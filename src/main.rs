e5778249484dfc69c98a82600911b94c150531c6
Rabin Phaiju <rabinphaiju15@gmail.com>
2023-01-14 10:19:35 +0545

    added threads

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 
 use core::num;
 use std::io;
+use std::sync::{Mutex, Arc};
 use rand::Rng;
 use std::io::{Write,BufReader,BufRead,ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 use std::ops::Add; // allows to add generics
 use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
+use std::rc::Rc;
 
 mod restaurant;
 use crate::restaurant::order_food;
@@ -510,12 +514,95 @@ fn box_s(){
 
 }
 
+fn concurrencys(){
+    // parallel program
+    // common problems with parallel programming
+    // 1. Thread area ccessing data in the wrong order
+    // 2. Threads are blocked from executing because of confusion
+    // 3. Over requirements to proceed with execution
+
+    let thread1 = thread::spawn(|| {
+        for i in 1..25{
+            println!("Spawn thread: {}",i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..20{
+        println!("Main thread  :{}",i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    thread1.join().unwrap();
+    
+}
+
+fn threadss(){
+    pub struct Bank{
+        balance: f32
+    }
+
+    fn withdraw(the_bank:&mut Bank, amt:f32){
+        the_bank.balance -= amt;
+    }
+
+    let mut bank = Bank{balance:100.0};
+    withdraw(&mut bank, 5.00);
+
+    println!("Balance : {}",bank.balance);
+
+    fn customer(the_bank: &mut Bank){
+        withdraw(the_bank, 5.00)
+    }
+
+    // thread::spawn(|| {
+    //     customer(&mut bank)
+    // }).join().unwrap();
+
+}
+
+fn rcts(){
+    pub struct Bank{
+        balance: f32
+    }
+
+    fn withdraw(the_bank: &Arc<Mutex<Bank>>,amt:f32){
+        let mut bank_ref = the_bank.lock().unwrap();
+        if bank_ref.balance < 5.00{
+            println!(" Current balance : {} is less",bank_ref.balance);
+        }else{
+            bank_ref.balance -= amt;
+            println!("Cus withdrew : {}",bank_ref.balance);
+        }
+    }
+
+    fn customer(the_bank:Arc<Mutex<Bank>>){
+        withdraw(&the_bank, 5.00);
+    }
+    let bank: Arc<Mutex<Bank>> = Arc::new(Mutex::new(Bank { balance: 20.00 }));
+    let handles = (0..10).map(|_| {
+        let bank_ref = bank.clone();
+        thread::spawn(|| {
+            customer(bank_ref)
+        })
+    });
+
+    for handle in handles{
+        handle.join().unwrap();
+    }
+
+    println!("Total {}",bank.lock().unwrap().balance);
+
+}
+
 
 fn main() {
 
-    concurrencys();
+    rcts();
     
-    box_s();
+    // threadss();
+    // concurrencys();
+    // box_s();
     // smart_pointer();
     // closures();
     // iterators();
